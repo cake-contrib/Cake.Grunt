398576b7f7ef4fdf57ea4ef443a1450f19ae4351
GitHub <noreply@github.com>
2021-01-14 20:32:53 +0000

    AppVeyor Publish: e86a0d35f1e5ad9e4d2a18bdd73549e1ac642d8b Merge pull request #16 from cake-contrib/remove-glitter-ref
    removed reference to glitter

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -1,133 +1,150 @@
 
-var camelCaseTokenizer = function (obj) {
+var camelCaseTokenizer = function (builder) {
+
+  var pipelineFunction = function (token) {
     var previous = '';
-    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
-        var current = cur.toLowerCase();
-        if(acc.length === 0) {
-            previous = current;
-            return acc.concat(current);
-        }
-        previous = previous.concat(current);
-        return acc.concat([current, previous]);
+    // split camelCaseString to on each word and combined words
+    // e.g. camelCaseTokenizer -> ['camel', 'case', 'camelcase', 'tokenizer', 'camelcasetokenizer']
+    var tokenStrings = token.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+      var current = cur.toLowerCase();
+      if (acc.length === 0) {
+        previous = current;
+        return acc.concat(current);
+      }
+      previous = previous.concat(current);
+      return acc.concat([current, previous]);
     }, []);
+
+    // return token for each string
+    // will copy any metadata on input token
+    return tokenStrings.map(function(tokenString) {
+      return token.clone(function(str) {
+        return tokenString;
+      })
+    });
+  }
+
+  lunr.Pipeline.registerFunction(pipelineFunction, 'camelCaseTokenizer')
+
+  builder.pipeline.before(lunr.stemmer, pipelineFunction)
 }
-lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
 var searchModule = function() {
+    var documents = [];
     var idMap = [];
-    function y(e) { 
-        idMap.push(e); 
+    function a(a,b) { 
+        documents.push(a);
+        idMap.push(b); 
     }
+
+    a(
+        {
+            id:0,
+            title:"GruntLocalRunner",
+            content:"GruntLocalRunner",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntLocalRunner',
+            title:"GruntLocalRunner",
+            description:""
+        }
+    );
+    a(
+        {
+            id:1,
+            title:"GruntRunnerAliases",
+            content:"GruntRunnerAliases",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntRunnerAliases',
+            title:"GruntRunnerAliases",
+            description:""
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"GruntGlobalRunner",
+            content:"GruntGlobalRunner",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntGlobalRunner',
+            title:"GruntGlobalRunner",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"GruntLocalRunnerSettings",
+            content:"GruntLocalRunnerSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntLocalRunnerSettings',
+            title:"GruntLocalRunnerSettings",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"GruntRunnerFactory",
+            content:"GruntRunnerFactory",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntRunnerFactory',
+            title:"GruntRunnerFactory",
+            description:""
+        }
+    );
+    a(
+        {
+            id:5,
+            title:"GruntRunner",
+            content:"GruntRunner",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntRunner_1',
+            title:"GruntRunner<TSettings>",
+            description:""
+        }
+    );
+    a(
+        {
+            id:6,
+            title:"GruntRunnerSettings",
+            content:"GruntRunnerSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Grunt/api/Cake.Grunt/GruntRunnerSettings',
+            title:"GruntRunnerSettings",
+            description:""
+        }
+    );
     var idx = lunr(function() {
-        this.field('title', { boost: 10 });
+        this.field('title');
         this.field('content');
-        this.field('description', { boost: 5 });
-        this.field('tags', { boost: 50 });
+        this.field('description');
+        this.field('tags');
         this.ref('id');
-        this.tokenizer(camelCaseTokenizer);
+        this.use(camelCaseTokenizer);
 
         this.pipeline.remove(lunr.stopWordFilter);
         this.pipeline.remove(lunr.stemmer);
-    });
-    function a(e) { 
-        idx.add(e); 
-    }
-
-    a({
-        id:0,
-        title:"GruntRunnerAliases",
-        content:"GruntRunnerAliases",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:1,
-        title:"GruntRunnerFactory",
-        content:"GruntRunnerFactory",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:2,
-        title:"GruntLocalRunner",
-        content:"GruntLocalRunner",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:3,
-        title:"GruntLocalRunnerSettings",
-        content:"GruntLocalRunnerSettings",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:4,
-        title:"GruntRunnerSettings",
-        content:"GruntRunnerSettings",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:5,
-        title:"GruntRunner",
-        content:"GruntRunner",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:6,
-        title:"GruntGlobalRunner",
-        content:"GruntGlobalRunner",
-        description:'',
-        tags:''
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntRunnerAliases',
-        title:"GruntRunnerAliases",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntRunnerFactory',
-        title:"GruntRunnerFactory",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntLocalRunner',
-        title:"GruntLocalRunner",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntLocalRunnerSettings',
-        title:"GruntLocalRunnerSettings",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntRunnerSettings',
-        title:"GruntRunnerSettings",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntRunner_1',
-        title:"GruntRunner<TSettings>",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Grunt/api/Cake.Grunt/GruntGlobalRunner',
-        title:"GruntGlobalRunner",
-        description:""
+        documents.forEach(function (doc) { this.add(doc) }, this)
     });
 
     return {
